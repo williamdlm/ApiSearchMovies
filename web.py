098a02156d01ddf0ea9f0f715e11cefe5efb2193
William David <30805462+williamdlm@users.noreply.github.com>
2024-04-22 15:17:24 +0100

    Update web.py

--- web.py
+++ web.py
@@ -2,84 +2,81 @@ from flask import Flask, jsonify, request, redirect
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from flask_cors import CORS
-#from urllib2 import urlopen
 import os   
 
 app = Flask(__name__)
 CORS(app)
-# cors = CORS(app, resources={
-#     r"/*":{
-#         "origins:" "*"
-#     }
-# })
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
 @app.route('/api/v1/filmes', methods=['GET'])
 def filmes():
     URL = "http://www.adorocinema.com/filmes/todos-filmes/notas-espectadores/"
-    
-    html_doc = urlopen(URL).read()
+    req = Request(URL, headers={'User-Agent': user_agent})
+    html_doc = urlopen(req).read()
     soup = BeautifulSoup(html_doc, "html.parser")
     data = []
-    for dataBox in soup.find_all("div", class_="data_box"):
-        titleObj = dataBox.find("a", class_="no_underline")
-        imgObj = dataBox.find(class_="img_side_content").find_all(class_="acLnk")[0]
-        sinopseObj = dataBox.find("div", class_="content").find_all("p")[0]
-        dateObj = dataBox.find("div", class_="content").find("div", class_="oflow_a")
-        movieLinkObj = dataBox.find(class_="img_side_content").find_all("a")[0]
-        generoObj = dataBox.find("div", class_="content").find_all('li')[3].find('div',class_="oflow_a")
+    for dataBox in soup.find_all("div", class_="card entity-card entity-card-list cf"):
+        titleObj = dataBox.find("a", class_="meta-title-link")
+        imgObj = dataBox.find("img",class_="thumbnail-img")
+        sinopseObj = dataBox.find("div", class_="synopsis")
+        dateObj = soup.find_all("span", class_="date")
+        movieLinkObj = dataBox.find("a", class_="meta-title-link")
         detailsLink = 'http://www.adorocinema.com' + movieLinkObj.attrs['href']
 
         #LOAD FULL SINOPSE 
-        htmldocMovieDetail = urlopen(detailsLink).read()
+        req2 = Request(detailsLink, headers={'User-Agent': user_agent})
+        htmldocMovieDetail = urlopen(req2).read()
         soupMovieDetail = BeautifulSoup(htmldocMovieDetail, "html.parser")
         fullSinopse = soupMovieDetail.find(class_="content-txt")     
         fullImgObj = soupMovieDetail.find("meta",  property="og:image")   
 
         data.append({'titulo': titleObj.text.strip(),
-                    'genero': generoObj.text.replace('\n','').strip(),
                     'poster' : fullImgObj["content"], 
                     'sinopse' : sinopseObj.text.strip(),
-                    'data' :  dateObj.text[0:11].strip(),
+                    'data' :  dateObj[0].text.strip(),
                     'link' : detailsLink,
-                    'sinopseFull': fullSinopse.text})
+                    'sinopseFull': fullSinopse.text
+                    })
                 
     return jsonify({'filmes': data})  
 
 @app.route('/api/v1/filmes/<page_id>', methods=['GET'])
 def NotasEspectadores(page_id):
     URL = "http://www.adorocinema.com/filmes/todos-filmes/notas-espectadores/?page={}".format(page_id)
-    
-    html_doc = urlopen(URL).read()
+    req = Request(URL, headers={'User-Agent': user_agent})
+    html_doc = urlopen(req).read()
     soup = BeautifulSoup(html_doc, "html.parser")
     data = []
-    for dataBox in soup.find_all("div", class_="data_box"):
-        titleObj = dataBox.find("a", class_="no_underline")
-        # imgObj = dataBox.find(class_="img_side_content").find_all(class_="acLnk")[0]
-        sinopseObj = dataBox.find("div", class_="content").find_all("p")[0]
-        dateObj = dataBox.find("div", class_="content").find("div", class_="oflow_a")
-        movieLinkObj = dataBox.find(class_="img_side_content").find_all("a")[0]
-        generoObj = dataBox.find("div", class_="content").find_all('li')[3].find('div',class_="oflow_a")
+    for dataBox in soup.find_all("div", class_="card entity-card entity-card-list cf"):
+        titleObj = dataBox.find("a", class_="meta-title-link")
+        sinopseObj = dataBox.find("div", class_="synopsis")
+        dateObj = soup.find_all("span", class_="date")
+        movieLinkObj = dataBox.find("a", class_="meta-title-link")
         detailsLink = 'http://www.adorocinema.com' + movieLinkObj.attrs['href']
 
         #LOAD FULL SINOPSE 
-        htmldocMovieDetail = urlopen(detailsLink).read()
+        req2 = Request(detailsLink, headers={'User-Agent': user_agent})
+        htmldocMovieDetail = urlopen(req2).read()
         soupMovieDetail = BeautifulSoup(htmldocMovieDetail, "html.parser")
         fullSinopse = soupMovieDetail.find(class_="content-txt")        
         fullImgObj = soupMovieDetail.find("meta",  property="og:image")   
 
         data.append({'titulo': titleObj.text.strip(),
-                    'genero': generoObj.text.replace('\n','').strip(),
                     'poster' : fullImgObj["content"], 
                     'sinopse' : sinopseObj.text.strip(),
-                    'data' :  dateObj.text[0:11].strip(),
+                    'data' :  dateObj[0].text.strip(),
                     'link' : detailsLink,
-                    'sinopseFull': fullSinopse.text})
+                    'sinopseFull': fullSinopse.text
+                    })
                 
     return jsonify({'filmes': data})    
 
 @app.route('/api/v1/filmes/emcartaz', methods=['GET'])
 def EmCartaz():
-    html_doc = urlopen("http://www.adorocinema.com/filmes/numero-cinemas/").read()
+    URL="http://www.adorocinema.com/filmes/numero-cinemas/"
+    req = Request(URL, headers={'User-Agent': user_agent})
+    html_doc = urlopen(req).read()
     soup = BeautifulSoup(html_doc, "html.parser")
 
     data = []
@@ -113,8 +110,8 @@ def filmes_api_page(page_id):
         
     else:
         url = "http://www.adorocinema.com/filmes/numero-cinemas/?page={}".format(page_id)
-        
-        html_doc = urlopen(url).read()
+        req = Request(URL, headers={'User-Agent': user_agent})
+        html_doc = urlopen(req).read()
         soup = BeautifulSoup(html_doc, "html.parser")
     
         data = []
