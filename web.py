56d51b2c16af837e3dc7299fc6fa10bfe9d9aa29
William David <30805462+williamdlm@users.noreply.github.com>
2024-04-22 15:21:12 +0100

    Update web.py

--- web.py
+++ web.py
@@ -133,6 +133,6 @@ def filmes_api_page(page_id):
                     
         return jsonify({'filmes': data})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(debug=True, host='0.0.0.0', port=port)
