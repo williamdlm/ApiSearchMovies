4cbdce836a0e8c8d12d26925ff8ff5bce4f36c0e
William David <30805462+williamdlm@users.noreply.github.com>
2024-04-22 15:21:58 +0100

    Update web.py

--- web.py
+++ web.py
@@ -6,11 +6,6 @@ import os
 
 app = Flask(__name__)
 CORS(app)
- cors = CORS(app, resources={
-     r"/*":{
-         "origins:" "*"
-     }
- })
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
